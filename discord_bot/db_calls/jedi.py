3db1fa83f7ea01c1f2c27556463fb10d8ad37ce5
Noelbiggs28 <noelbiggs28@gmail.com>
2024-07-04 09:20:59 -1000

    jedi shows all units even if mission is impossible

--- discord_bot/db_calls/jedi.py
+++ discord_bot/db_calls/jedi.py
@@ -28,21 +28,6 @@ def jedi_plan():
                             FROM platoons p 
                             LEFT JOIN units u ON p.character_name = u.character_name
                             WHERE p.phase < 4  
-                            AND NOT EXISTS (
-                                SELECT 1
-                                FROM platoons p2 
-                                LEFT JOIN units u2 ON p2.character_name = u2.character_name
-                                WHERE p2.phase < 4
-                                    AND (
-                                    (p.phase = 1 AND (u2.r5 IS NULL OR u2.r5 = 0))
-                                    OR (p.phase = 2 AND (u2.r6 IS NULL OR u2.r6 = 0))
-                                    OR (p.phase = 3 AND (u2.r7 IS NULL OR u2.r7 = 0))
-                                    OR (p.phase = 4 AND (u2.r8 IS NULL OR u2.r8 = 0))
-                                    OR (p.phase > 4 AND (u2.r9 IS NULL OR u2.r9 = 0))
-                                )
-                                AND p2.operations = p.operations
-                                AND p2.planet = p.planet
-                            )
                             GROUP BY p.planet,p.character_name,p.phase, u.r5, u.r6, u.r7, u.r8
                             ) as a
                             WHERE a.r_value < a.count
@@ -53,7 +38,7 @@ def jedi_plan():
     needed_units = {'needed_units': units if units else "doable"}
     if needed_units['needed_units']== "doable":
         return "all misions doable"
-    message = f"Units where we have less than needed after subtracting impossible missions\n"
+    message = f"Units where needed - have > 0\n"
     planets = set()
     for unit in needed_units['needed_units']:
         relic_words = "Relic 5+" if unit[3] == 1 else "Relic 6+" if unit[3] == 2 else "Relic 7+" if unit[3] == 3 else "Relic 8+" if unit[3] == 4 else "relic 9"
@@ -63,11 +48,12 @@ def jedi_plan():
     sorted_planets = sorted(planets, key=lambda x: order_index[x[0]])
 
     for planet in sorted_planets:
-        message += f'{planet[0]}\n{planet[1]}\n'
-        for unit in needed_units['needed_units']:
-            if unit[0] == planet[0]:
-                message += f"Unit: {unit[1]}, Have: {unit[4]} Need: {unit[5]} Operations: {''.join(unit[2])}\n"
-        message +="\n"
+        if planet[0] != "Zeffo":
+            message += f'{planet[0]}\n{planet[1]}\n'
+            for unit in needed_units['needed_units']:
+                if unit[0] == planet[0]:
+                    message += f"Unit: {unit[1]}, Have: {unit[4]} Need: {unit[5]} Operations: {''.join(unit[2])}\n"
+            message +="\n"
         
     return message
                             # AND NOT EXISTS (
@@ -82,3 +68,6 @@ def jedi_plan():
                             #         OR (p.phase = 4 AND (u2.r8 IS NULL OR u2.r8 = 0))
                             #         OR (p.phase > 4 AND (u2.r9 IS NULL OR u2.r9 = 0))
                             #     )
+                            #     AND p2.operations = p.operations
+                            #     AND p2.planet = p.planet
+                            # )
